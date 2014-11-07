c8889c1d0197a6d0b265eaca9bcc309d2669ac70
arielmmonestel <arielmmonestel@gmail.com>
2014-11-06 21:08:37 -0600

    Graficador Turing(Del profe)

---
+++ main.cpp
@@ -0,0 +1,24 @@
+##include <iostream>
+#include "include\GraficadorTuring.h"
+#include <sstream>
+
+using namespace std;
+
+int main()
+{
+    int cant = 7;
+    GraficadorTuring g(800, 650, cant);
+    for (int i = 0; i < cant; i++) {
+        stringstream str;
+        str << "lol" << i;
+        g.asignarNombreEstado(i, str.str());
+    }
+    for (int i = 0; i < cant; i++) {
+        for (int j = 0; j < cant; j++) {
+            g.asignarTransicion(i, j, "wut");
+        }
+    }
+    g.dibujarEstados();
+    getch();
+    return 0;
+}
